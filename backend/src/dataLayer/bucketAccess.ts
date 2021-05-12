2aa23a8130378987b8b9f6fc2634cae033efc382
Joseph Hanna <joseph.com.ec@gmail.com>
2021-05-11 20:08:16 -0500

    Add tracing

--- backend/src/dataLayer/bucketAccess.ts
+++ backend/src/dataLayer/bucketAccess.ts
@@ -1,13 +1,18 @@
 import * as AWS from "aws-sdk"
+import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from "../utils/logger"
 
 
 const bucketName = process.env.TODOS_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 
-const s3 = new AWS.S3({
+const XAWS = AWSXRay.captureAWS(AWS)
+const s3 = new XAWS.S3({
     signatureVersion: 'v4'
 })
+
+const logger = createLogger('bucketAccess')
 
 export function getUploadUrl(attachId: string) {
     return s3.getSignedUrl('putObject', {
@@ -18,8 +23,18 @@ export function getUploadUrl(attachId: string) {
 }
 
 export function deleteFile(attachId: string) {
-    return s3.deleteObject({
+
+    const params = {
         Bucket: bucketName,
-        Key: attachId
+        Delete: {
+            Objects: [
+                { Key: attachId }
+            ]
+        }
+    }
+
+    s3.deleteObjects(params, function (err, data) {
+        if (err) logger.error(err);
+        else logger.info("Delete File Success " + data)
     })
 }
--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -6,12 +6,13 @@ import { TodoUpdateParams } from '../models/TodoUpdateParam'
 import { createLogger } from '../utils/logger'
 
 const logger = createLogger('todosAccess')
-
+const AWSXRay = require('aws-xray-sdk');
+const XAWS = AWSXRay.captureAWS(AWS)
 
 export class TodoAccess {
 
     constructor(
-        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
         private readonly todosTable = process.env.TODOS_TABLE,
     ) { }
 
